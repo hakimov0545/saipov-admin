9de953d46e6578d23814b56a2220690fcb37c489
Rahmatullayev Suhrob <108258306+sarbondev@users.noreply.github.com>
2025-07-16 22:55:40 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+// const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://server.saipovgroup.uz/api"
 
 class ApiService {
   constructor() {
